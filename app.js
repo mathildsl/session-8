8e99ea9e058950cb55a5eda32168c209f95065fb
mathildsl <mathildsl@users.noreply.github.com>
2017-10-24 16:15:38 +0200

    session 8

--- app.js
+++ app.js
@@ -1,45 +1,61 @@
 var app = new Vue({
   el: '#app',
   data: {
-    firstName: "Jules",
-    lastName: "Bonnard",
-    description: "Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
-    photo: "https://pbs.twimg.com/profile_images/899285838516936704/nfeLD7he_400x400.jpg",
-    twitter: "julesbonnard",
-    github: "julesbonnard",
-    mail: "julesbonnard@gmail.com",
+    firstName: "Mathilde",
+    lastName: "Saliou",
+    description: "Je suis journaliste. J'aime bien l'égalité femmes/hommes, la tech et les libertés.",
+    photo: "https://pbs.twimg.com/profile_images/378800000566983440/2fff14100c36807756871026646783fa_400x400.jpeg",
+    twitter: "mathildsl",
+    github: "mathildsl",
+    mail: "saliou.mathilde@gmail.com",
     experiences: [{
-      dateBegin: "2014",
+      dateBegin: "2017",
       dateEnd: "Today",
-      name: "Agence France Presse",
-      title: "Datajournalist",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      name: "RFI",
+      title: "Journaliste, en alternance",
+      logo: "https://pbs.twimg.com/profile_images/542360181930090496/VTySt9uU_400x400.png",
+      description: "Contrat d'apprentissage auprès de l'équipe de l'Atelier des Médias / Mondoblog."
     }, {
-      dateBegin: "Été 2013",
+      dateBegin: "2017",
+      dateEnd: "Today",
+      name: "Planet Newsletter",
+      title: "Co-fondatrice",
+      logo: "https://pbs.twimg.com/profile_images/876481293948768259/c9bWjeHR_400x400.jpg",
+      description: "Planet, la newsletter qui décortique une actu tech ou business chaque matin, par mail ou messenger.",
+    }, {
+      dateBegin: "2017",
       dateEnd: false,
-      name: "Le Monde.fr",
-      title: "Data-journalist",
-      description: false
+      name: "Divers (L'Etudiant, Cheek magazine, etc)",
+      logo: false,
+      title: "Journaliste free-lance",
     }, {
-      dateBegin: "2012",
-      dateEnd: "2013",
-      name: "Le Dauphiné Libéré",
-      title: "Stagiaire et correspondant de presse",
-      description: false
+      dateBegin: "Mai 2017",
+      dateEnd: "Août 2017",
+      name: "Cheek Magazine",
+      logo: false,
+      title: "Journaliste, stagiaire",
     }],
     formations: [{
-      dateBegin: "2012",
-      dateEnd: "2014",
-      name: "Master Journalisme",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2016",
+      dateEnd: "2018",
+      name: "Master à l'Ecole de Journalisme",
+      university: "Sciences Po Paris",
+      logo: "https://pbs.twimg.com/profile_images/781539022283964416/or_LcokZ_400x400.jpg"
     }, {
-      dateBegin: "2009",
-      dateEnd: "2012",
-      name: "Bachelor Sciences Politiques",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2013",
+      dateEnd: "2016",
+      name: "Master à l'Ecole de la Communication",
+      university: "Sciences Po Paris",
+      logo: "https://pbs.twimg.com/profile_images/781539022283964416/or_LcokZ_400x400.jpg"
+    },{
+      dateBegin: "2010",
+      dateEnd: "2013",
+      name: "Bachelor de Sciences Politiques",
+      university: "Sciences Po Paris",
+      logo: "https://pbs.twimg.com/profile_images/781539022283964416/or_LcokZ_400x400.jpg"
     }],
-    languages: ["Anglais"],
-    skills: ["Node", "VueJS", "D3js", "SCSS", "HTML"]
+    languages: ["Anglais, Allemand, Espagnol"],
+    skills: ["CSS", "HTML"]
   },
   computed: {
     fullName() {
